064c357ff5391e4342496924d73087723d6505b3
pesaljmartin <mpesalj@spark451.com>
2019-09-17 18:32:39 +0200

    class to func and axios, simple fetch

--- src/containers/Flights/index.js
+++ src/containers/Flights/index.js
@@ -1,63 +1,56 @@
-import React, { Component } from "react";
-import axios from "axios";
+import React, { useState, useEffect } from "react";
 
 import FlightsWrapper from "../../components/Flights/FlightsWrapper";
 
-class Flights extends Component {
-  state = {
-    flights: []
-  };
+const Flights = () => {
+  const [flights, setFlights] = useState([]);
 
-  componentDidMount() {
-    this.getAllFlights();
-  }
-
-  getAllFlights = async () => {
-    try {
-      const response = await axios.get("./data/index.json");
-      this.setState({
-        flights: response.data
+  useEffect(() => {
+    fetch("./data/index.json")
+      .then(response => response.json())
+      .then(data => {
+        setFlights(data);
+      })
+      .catch(function(error) {
+        console.log(error);
       });
-    } catch (error) {
-      console.error(error);
+  }, []);
+
+  const sortByName = direction => {
+    if (direction) {
+      flights.sort((a, b) => {
+        return a.origin.value > b.origin.value ? 1 : -1;
+      });
+    } else {
+      flights.sort((a, b) => {
+        return a.origin.value > b.origin.value ? -1 : 1;
+      });
     }
+
+    setFlights([...flights]);
   };
 
-  sortByName = direction => {
-    const { flights } = this.state;
-    direction
-      ? flights.sort((a, b) => {
-          return a.origin.value > b.origin.value ? 1 : -1;
-        })
-      : flights.sort((a, b) => {
-          return a.origin.value > b.origin.value ? -1 : 1;
-        });
+  const sortByTime = direction => {
+    if (direction) {
+      flights.sort((a, b) => {
+        return a.departure - b.departure;
+      });
+    } else {
+      flights.sort((a, b) => {
+        return b.departure - a.departure;
+      });
+    }
 
-    this.setState({ flights });
+    setFlights([...flights]);
   };
 
-  sortByTime = direction => {
-    const { flights } = this.state;
-    direction
-      ? flights.sort((a, b) => {
-          return a.departure - b.departure;
-        })
-      : flights.sort((a, b) => {
-          return b.departure - a.departure;
-        });
-
-    this.setState({ flights });
-  };
-
-  render() {
-    return (
-      <FlightsWrapper
-        flights={this.state.flights}
-        sortByName={this.sortByName}
-        sortByTime={this.sortByTime}
-      />
-    );
-  }
-}
+  return (
+    <FlightsWrapper
+      flights={flights}
+      sortByName={sortByName}
+      sortByTime={sortByTime}
+    />
+  );
+};
 
 export default Flights;
